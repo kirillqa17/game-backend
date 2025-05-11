354d85c71aaa8d35178ab143d0e48a21a0b63f1b
Kirill Zakharov <dungeonmaster070507@gmail.com>
2025-05-12 01:52:12 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -226,7 +226,7 @@ async fn main() -> std::io::Result<()> {
             .service(update_record)
 
     })
-    .bind_openssl("0.0.0.0:4443", builder)?
+    .bind_openssl("0.0.0.0:1904", builder)?
     .run()
     .await
 }
